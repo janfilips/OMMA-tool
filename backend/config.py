1a2ebde44c84bf7e180ae9b251d94b17974ab7ee
Jan Filips <janfilipsgt@gmail.com>
2024-02-19 16:02:02 +0200

    Add environment variables for development, staging, and production

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,10 @@ CODE_DIR = os.path.dirname(__file__)
 ROOT_DIR = os.path.dirname(CODE_DIR)
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 
+DEVELOPMENT = os.getenv("DEVELOPMENT", None)
+STAGING = os.getenv("STAGING", None)
+PRODUCTION = os.getenv("PRODUCTION", None)
+
 SECRET_KEY = os.getenv("SECRET_KEY", "h21k3jhk12h3k1hk23hkjqhkdhaskhdksahdiuyi2y7fydweifusehfksdhfkjh")
 
 origins_str = os.getenv("CORS_ORIGINS", "")
@@ -11,7 +15,6 @@ if origins_str:
     CORS_ORIGINS = origins_str.split(",")
 else:
     CORS_ORIGINS = []
-
 
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
 POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", "5432"))
