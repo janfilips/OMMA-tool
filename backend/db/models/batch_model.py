076411dca3ad996ff42ca497912f49c14a1117ca
Jan Filips <janfilipsgt@gmail.com>
2024-02-20 10:54:23 +0200

    Refactor batch_model.py: Remove unnecessary indexes

--- backend/db/models/batch_model.py
+++ backend/db/models/batch_model.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 class Batch(Base):
     __tablename__ = 'batch'
     
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
 
     measurements = relationship("Measurement", back_populates="batch")
