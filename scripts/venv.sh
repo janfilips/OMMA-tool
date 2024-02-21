e57d3929a99ba34e57c458c417b457d834df9097
Jan Filips <janfilipsgt@gmail.com>
2024-02-21 11:07:14 +0200

    Update .env file reference in venv.sh script

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -4,7 +4,7 @@ python3.11 -m venv .venv
 source .venv/bin/activate
 
 # Load environment variables from the .env file
-export $(grep -v '^#' .env.devel | xargs)
+export $(grep -v '^#' .env.local | xargs)
 
 poetry install
 
